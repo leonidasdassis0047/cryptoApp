55554dde2b260eb8c4dacd78f6fa65bf1fc5eddc
leonidasdassis0047 <leonidasdassis@gmail.com>
2022-06-01 18:01:31 +0300

    Added param handlerfor cryptos controller, and handling single crypto details controller

--- source/resources/cryptos/controller.ts
+++ source/resources/cryptos/controller.ts
@@ -11,12 +11,31 @@ export class CryptoController implements IController {
   }
 
   private initialiseRoutes = () => {
+    this.router.param(
+      'cryptoId',
+      (request: Request, response: Response, next: NextFunction): void => {
+        console.log(
+          'handling router param for crypto: ',
+          request.params.cryptoId
+        );
+        next();
+      }
+    );
+
     this.router
       .route(this.path)
       .get(AsyncHandler(this.getCryptosInRealtime.bind(this)));
+
+    this.router
+      .route(this.path + '/:cryptoId')
+      .get(AsyncHandler(this.getCryptoDetails.bind(this)));
   };
 
   //   ==========================================
+  //   controllers below
+  //   ==========================================
+
+  //   get all cryptos in realtime.
   private getCryptosInRealtime = async (
     request: Request,
     response: Response,
@@ -24,6 +43,19 @@ export class CryptoController implements IController {
   ): Promise<void> => {
     try {
       response.status(200).send('cryptos incoming');
+    } catch (error: any) {
+      next(error);
+    }
+  };
+
+  //   get single crypto information
+  private getCryptoDetails = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      response.status(200).send('single crypto incoming');
     } catch (error: any) {
       next(error);
     }
